84fb6a6f00993daa1120cb36c4297482180be94c
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-11-30 04:37:11 +0500

    dome changes

---
+++ N73HomeTask.Domain/Common/Exceptions/FuncResult.cs
@@ -0,0 +1,10 @@
+﻿namespace N73HomeTask.Domain.Common.Exceptions;
+
+public class FuncResult<T>
+{
+    public T Data { get; set; }
+    public Exception? Exception { get; set; }
+    public bool IsSuccess => Exception is null;
+    public FuncResult(T data) => Data = data;
+    public FuncResult(Exception ex) => Exception = ex;
+}
