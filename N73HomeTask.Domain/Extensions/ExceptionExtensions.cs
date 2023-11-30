3f9b48345cc097cfea68817ab47a99e02bdd298a
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-11-30 05:05:50 +0500

    created FuncResult successfully

---
+++ N73HomeTask.Domain/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using N73HomeTask.Domain.Common.Exceptions;
+
+namespace N73HomeTask.Domain.Extensions;
+
+public static class ExceptionExtensions
+{
+    public static async ValueTask<FuncResult<T>> GetValuesAsync<T>(this Func<Task<T>> func) where T : struct
+    {
+        FuncResult<T> result;
+        try
+        {
+            result = new FuncResult<T>(await func());
+        }
+        catch (Exception e)
+        {
+            result = new FuncResult<T>(e);
+        }
+
+        return result;
+    }
+    public static async ValueTask<FuncResult<T>> GetValuesAsync<T>(this Func<ValueTask<T>> func) where T : struct
+    {
+        FuncResult<T> result;
+        try
+        {
+            result = new FuncResult<T>(await func());
+        }
+        catch (Exception e)
+        {
+            result = new FuncResult<T>(e);
+        }
+
+        return result;
+    }
+}
