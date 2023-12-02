586ddfe084a9fce88ac35a71e8a2edbde6d813f8
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-12-03 04:29:08 +0500

    there is some thing of in it

--- ConsoleApp1/GeneratedCode.cs
+++ ConsoleApp1/GeneratedCode.cs
@@ -1,8 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 namespace ConsoleApp1;
-
 public static class GeneratedCode
 {
-    [InterceptsLocation("""dhfdsjf""",)]
     public static void MyInterceptor(this Car car) => Console.WriteLine("Car is stopping");
 }
