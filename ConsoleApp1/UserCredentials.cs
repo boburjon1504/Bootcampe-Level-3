7e60f204dffc79a2b3f9246875d32208ac0eefe7
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-12-03 04:38:45 +0500

    good first issue

---
+++ ConsoleApp1/UserCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+public class UserCredentials
+{
+    public Guid UserId { get; set; }
+    public string Password { get; set; }
+    public Guid Id { get; set; }
+}
