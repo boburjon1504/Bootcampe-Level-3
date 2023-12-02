b2a913769eef52de4a5842f5f08de36c3525ea09
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-12-03 04:27:48 +0500

    a little bit of practice

---
+++ WebApplication1/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1;
+
+public class Book : IEntity
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Title { get; set; } = default!;
+}
